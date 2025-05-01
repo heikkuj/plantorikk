bbec560a07fb61e5208e83d14c6cbcc2e9d66e05
heikkuj <heikkuamo@gmail.com>
2025-05-01 22:24:41 +0200

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,124 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+// import { supabase } from '@/config/initSupabase';
+// import React, { useState } from 'react';
+// import { Alert, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import Spinner from 'react-native-loading-spinner-overlay';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+// const Login = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [loading, setLoading] = useState(false);
 
-export default function HomeScreen() {
+//   // Sign in with email and password
+//   const onSignInPress = async () => {
+//     setLoading(true)
+
+//     const { error } = await supabase.auth.signInWithPassword({
+//       email,
+//       password,
+//     })
+
+//     if (error) Alert.alert(error.message)
+//     setLoading(false)
+//   }
+
+//   // Create a new user
+//   const onSignUpPress = async () => {
+//     setLoading(true)
+//     const { error } = await supabase.auth.signUp({
+//       email: email,
+//       password: password,
+//     })
+
+//     if (error) Alert.alert(error.message)
+//     setLoading(false)
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Spinner visible={loading} />
+
+//       <Text style={styles.header}>My Cloud</Text>
+
+//       <TextInput
+//         autoCapitalize="none"
+//         placeholder="john@doe.com"
+//         value={email}
+//         onChangeText={setEmail}
+//         style={styles.inputField}
+//       />
+//       <TextInput
+//         placeholder="password"
+//         value={password}
+//         onChangeText={setPassword}
+//         secureTextEntry
+//         style={styles.inputField}
+//       />
+
+//       <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+//         <Text style={{ color: '#fff' }}>Sign in</Text>
+//       </TouchableOpacity>
+//       <Button onPress={onSignUpPress} title="Create Account" color={'#fff'}></Button>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: 200,
+//     padding: 20,
+//     backgroundColor: '#151515',
+//   },
+//   header: {
+//     fontSize: 30,
+//     textAlign: 'center',
+//     margin: 50,
+//     color: '#fff',
+//   },
+//   inputField: {
+//     marginVertical: 4,
+//     height: 50,
+//     borderWidth: 1,
+//     borderColor: '#2b825b',
+//     borderRadius: 4,
+//     padding: 10,
+//     color: '#fff',
+//     backgroundColor: '#363636',
+//   },
+//   button: {
+//     marginVertical: 15,
+//     alignItems: 'center',
+//     backgroundColor: '#2b825b',
+//     padding: 12,
+//     borderRadius: 4,
+//   },
+// })
+
+// export default Login
+
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#fffbea',
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  text: {
+    color: '#fff',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
   },
 });
---
+++ config/initSupabase.ts
@@ -0,0 +1,13 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import 'react-native-url-polyfill/auto';
+
+const url = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const key = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const supabase = createClient(url, key, {
+    auth: {
+        storage: AsyncStorage,
+        detectSessionInUrl: false,
+    },
+});
