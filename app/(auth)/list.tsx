eebc3e997dc91a47807037140f0d5d38bda2a373
heikkuj <heikkuamo@gmail.com>
2025-05-04 22:15:50 +0200

    alternate upload function

--- app/(auth)/list.tsx
+++ app/(auth)/list.tsx
@@ -3,6 +3,8 @@ import { supabase } from '@/config/initSupabase';
 import { useAuth } from '@/provider/AuthProvider';
 import { Ionicons } from '@expo/vector-icons';
 import { FileObject } from '@supabase/storage-js';
+import { decode } from 'base64-arraybuffer';
+import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from 'expo-image-picker';
 import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
@@ -12,101 +14,60 @@ const list = () => {
   const [images, setImages] = useState<FileObject[]>([]);
 
   useEffect(() => {
-    if (!user) return
+    if (!user) return;
 
     // Load user images
-    loadImages()
+    loadImages();
   }, [user]);
 
   const loadImages = async () => {
-    const { data } = await supabase.storage.from('images').list(user!.id)
+    const { data } = await supabase.storage.from('images').list(user!.id);
     if (data) {
-      setImages(data)
+      setImages(data);
     }
-  }
+  };
 
   const onSelectImage = async () => {
-    try {
-      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    const options: ImagePicker.ImagePickerOptions = {
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+    };
 
-      if (status !== 'granted') {
-        alert('Appen krever tillatelse for å laste opp bilder fra kamerarullen.');
-        return;
-      }
+    const result = await ImagePicker.launchImageLibraryAsync(options);
 
-      const result = await ImagePicker.launchImageLibraryAsync({
-        allowsEditing: true,
-        quality: 0.8,
-      });
-
-      if (result.canceled || !result.assets || result.assets.length === 0) {
-        return;
-      }
-
-      // Get selected image
-      const selectedImage = result.assets[0];
-
-      // Generate unique filename
-      const fileName = `${Date.now()}-${Math.floor(Math.random() * 1000)}`;
-      const fileExtenion = selectedImage.uri.split('.').pop();
-      const filePath = `${fileName}.${fileExtenion}`;
-
-      const response = await fetch(selectedImage.uri);
-      const blob = await response.blob();
-
-      // Upload to Supabase
-      const { data, error } = await supabase.storage
-      .from('images')
-      .upload(`${user!.id}/${filePath}`, blob, {
-        contentType: `image/${fileExtenion}`,
-        cacheControl: '3600',
-      });
-
-      if (error) {
-        console.error('Kunne ikke laste opp bildet.', error);
-        alert('Kunne ikke laste opp bildet. Vennligst prøv igjen.');
-        return;
-      }
-
+    // Save image if not cancelled
+    if (!result.canceled) {
+      const img = result.assets[0];
+      const base64 = await FileSystem.readAsStringAsync(img.uri, { encoding: 'base64' });
+      const filePath = `${user!.id}/${new Date().getTime()}.${img.type === 'image' ? 'png' : 'mp4'}`;
+      const contentType = img.type === 'image' ? 'image/png' : 'video/mp4';
+      await supabase.storage.from('images').upload(filePath, decode(base64), { contentType });
       loadImages();
-      
-
-    } catch (error) {
-      console.error('Error selecting/uploading image.');
-      alert('En feil oppstod. Vennligst prøv igjen.');
     }
-
-  }
+  };
 
   const onRemoveImage = async (item: FileObject, listIndex: number) => {
-    supabase.storage.from('images').remove([`${user!.id}/${item.name}`])
-    const newImages = [...images]
-    newImages.splice(listIndex, 1)
-    setImages(newImages)
-  }
-  
+    supabase.storage.from('images').remove([`${user!.id}/${item.name}`]);
+    const newFiles = [...images];
+    newFiles.splice(listIndex, 1);
+    setImages(newFiles);
+  };
 
   return (
     <View style={styles.container}>
       <ScrollView>
         {images.map((item, index) => (
-          <ImageItem
-            key={item.id}
-            item={item}
-            userId={user!.id}
-            onRemoveImage={() => onRemoveImage(item, index)}
-          />
+          <ImageItem key={item.id} item={item} userId={user!.id} onRemoveImage={() => onRemoveImage(item, index)} />
         ))}
       </ScrollView>
-  
+
       {/* FAB to add images */}
       <TouchableOpacity onPress={onSelectImage} style={styles.fab}>
         <Ionicons name="camera-outline" size={30} color={'#fff'} />
       </TouchableOpacity>
     </View>
-  )
-  
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -126,6 +87,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#2b825b',
     borderRadius: 100,
   },
-})
+});
 
-export default list
+export default list;
